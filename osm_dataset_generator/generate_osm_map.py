9d54e984a25d1efdcd58aff3fcdb07a3afb35849
hydrargyrum5799 <hgoel1000p@gmail.com>
2023-02-05 19:56:42 -0500

    creating semantic map queries for different universities

--- osm_dataset_generator/generate_osm_map.py
+++ osm_dataset_generator/generate_osm_map.py
@@ -187,7 +187,14 @@ def OverpassOSMQuery(params, origin, semantic_data_filename):
         geojson.dump(feature_collection, f)
 
     return True
-    
+
+def args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--semantic_data_path', type=str, default='semantic_data/', help='YAML file containing semantic objects to query')
+
+    args = parser.parse_args()
+    return args
 
 if __name__ == '__main__':
     params_filename = 'semantic_objects.yaml'
@@ -196,9 +203,40 @@ if __name__ == '__main__':
     with open(params_filename, 'r') as file:
         params = yaml.safe_load(file)
 
-    origin = {'lat': 39.945827951386065, 'lon': -75.2047706396303} # UPenn
-    # origin = {'lat': 40.74050005471615, 'lon': -74.1759877275644} # New York
-    # origin = {'lat': 41.381775552849454, 'lon': -73.96846537685275} # West Point
+    # List of origins
+    origins = [{'lat': 39.945827951386065, 'lon': -75.2047706396303}, # 1. UPenn
+              {'lat': 40.74050005471615, 'lon': -74.1759877275644}, # 2. New York
+              {'lat': 41.381775552849454, 'lon': -73.96846537685275}, # 3. West Point
+              {'lat': 42.3601, 'lon': -71.0942}, # 4. MIT
+              {'lat': 42.3770, 'lon': -71.1167}, # 5. Harvard
+              {'lat': 37.4275, 'lon': -122.1697}, # 6. Stanford
+              {'lat': 41.3163, 'lon': -72.9223},# 7. Yale
+              {'lat': 40.3431, 'lon': -74.6551}, # 8. Princeton
+              {'lat': 34.1377, 'lon': -118.1253}, # 9. Caltech
+              {'lat': 37.8719, 'lon': -122.2635}, # 10. UCB
+              {'lat': 34.0689, 'lon': -118.4452}, # 11. UCLA
+              {'lat': 34.0224, 'lon': -118.2851}, #12. USC
+              {'lat': 40.7295, 'lon': -73.9965}, # 13. NYU
+              {'lat': 40.8075, 'lon': -73.9626}, # 14. Columbia
+              {'lat': 42.4534, 'lon': -76.4735}, # 15. Cornell
+              {'lat': 40.4432, 'lon': -79.9428}, # 16. CMU
+              {'lat': 47.6553, 'lon': -122.3035}, # 17. UWash
+              {'lat': 42.2780, 'lon': -83.7382}, # 18. Michigan
+              {'lat': 33.7756, 'lon': -84.3963}, # 19. GATech
+              {'lat': 51.7548, 'lon': -1.2544}, # 20. Oxford
+              {'lat': 52.2054, 'lon': +0.1132}, # 21. Cambridge
+              {'lat': 51.4988, 'lon': -0.1749}, #22. ICL
+              {'lat': 47.3764, 'lon': +8.5476}, # 23. ETH Zurich
+              {'lat': 1.2966, 'lon': +103.7764}, # 24. NUS
+              {'lat': 1.3483, 'lon': +103.6831}] # 25. NTU
 
-    semantic_data_filename = 'leaflet_geojson_viz/data/semantic_data.json'
-    OverpassOSMQuery(params, origin, semantic_data_filename)
+    args  = args()
+
+    import os
+    if not os.path.exists(args.semantic_data_path):
+        os.makedirs(args.semantic_data_path)
+
+    for j,coord in enumerate(origins):
+        semantic_data_filename = args.semantic_data_path + 'semantic_data{}.json'.format(j)
+        #semantic_data_filename = 'leaflet_geojson_viz/data/semantic_data.json'
+        OverpassOSMQuery(params, coord, semantic_data_filename)
