8e97fb48cf029799f9639d261c456cdf8706f46f
wvat <vatnsdal@seas.upenn.edu>
2023-08-09 16:25:42 -0400

    Add class

--- cppsrc/telecom/tx/udp_tx.cpp
+++ cppsrc/telecom/tx/udp_tx.cpp
@@ -7,19 +7,23 @@
 #include <net/if.h>
 #include <vector> 
 
-bool sendMulticastMessage(const std::vector<float>& message) {
+UDP_TX::UDP_TX(std::string ifname){
+
+    // TODO change string literal
+    std::string ifname_ = ifname;
+
     // Create a socket
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd == -1) {
+    sockfd_ = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd_ == -1) {
         std::cerr << "Failed to create socket." << std::endl;
-        return false;
+        return;
     }
 
     // Set the interface for the socket to bind to
-    if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, "adhoc", strlen("adhoc")) == -1) {
+    if (setsockopt(sockfd_, SOL_SOCKET, SO_BINDTODEVICE, "adhoc", strlen("adhoc")) == -1) {
         std::cerr << "Failed to set socket options." << std::endl;
-        close(sockfd);
-        return false;
+        close(sockfd_);
+        return;
     }
 
     // Set up server address
@@ -27,22 +31,31 @@ bool sendMulticastMessage(const std::vector<float>& message) {
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(901); // Set the desired port (900 in this case)
     serverAddr.sin_addr.s_addr = inet_addr("224.0.0.251"); // Set the desired IP address (224.0.0.251 in this case)
+}
+
+UDP_TX::~UDP_TX(){
+    // Close the socket
+    close(sockfd_);
+}
+
+void UDP_TX::Transmit(const std::vector<float>& message) {
 
     // Prepare the message to be sent
     size_t messageLength = message.size() * sizeof(float);
 
     // Send the message
-    ssize_t numBytesSent = sendto(sockfd, reinterpret_cast<const char*>(message.data()), messageLength, 0, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr));
+    ssize_t numBytesSent = sendto(sockfd_, reinterpret_cast<const char*>(message.data()), messageLength, 0, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr));
     if (numBytesSent == -1) {
         std::cerr << "Failed to send message." << std::endl;
-        close(sockfd);
-        return false;
+        close(sockfd_);
+        return;
     }
 
     std::cout << "Sent message of size: " << messageLength << " bytes." << std::endl;
+    // TODO put a sleep variable
+    return;
+}
 
-    // Close the socket
-    close(sockfd);
-
-    return true;
+void UDP_TX::Trigger(std::shared_ptr<std::vector<float>> message){
+    return;
 }
--- cppsrc/telecom/tx/udp_tx.h
+++ cppsrc/telecom/tx/udp_tx.h
@@ -1,7 +1,19 @@
 #ifndef MULTICASTSENDER_H
 #define MULTICASTSENDER_H
+#include <memory>
 #include <vector>
+#include <cstring>
 
-bool sendMulticastMessage(const std::vector<float>& message);
+class UDP_TX{
+    private:
+        int sockfd_;
+        std::unique_ptr<std::vector<float>> tx_msg_ptr_;
 
+    public:
+        UDP_TX(std::string ifname = "adhoc");
+        ~UDP_TX();
+        void Transmit(const std::vector<float>& message);
+        void Trigger(std::shared_ptr<std::vector<float>> message);
+        // TODO get position from done()
+}
 #endif // MULTICASTSENDER_H
