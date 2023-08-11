efb84603153a282cc604e343007921afd6f8de36
wvat <vatnsdal@seas.upenn.edu>
2023-08-11 17:35:41 -0400

    Add boost program options

--- cppsrc/buf_com/main.cpp
+++ cppsrc/buf_com/main.cpp
@@ -6,15 +6,63 @@
 #include <vector> 
 #include <random>
 #include <iostream>
+#include <boost/program_options.hpp>
 
-int main(){
+namespace CC = CoverageControl;
+namespace po = boost::program_options;
 
-    CoverageControl::BufferHandler buf;
-    CoverageControl::UDP_TX udptx;
+int main(int argc, char* argv[]){
+    //std::cout << "Hello World" << std::endl;
 
-    buf.Trigger(udptx);
-    udptx.Transmit();
+    bool isTX;
+    bool isRX;
+
+    // argument handling
+    try{
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help", "show help message")
+            ("tx", "set TX flag")
+            ("rx", "set RX flag");
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+
+        if (vm.count("help")) {
+            std::cout << desc << "\n";
+            return 0;
+        }
+        
+        if(vm.count("tx")){
+            std::cout << "TX flag set" << std::endl;
+            isTX = true;
+        }
+        if(vm.count("rx")){
+            std::cout << "RX flag set" << std::endl;
+            isRX = true;
+        }
+    } catch(const po::error &e){
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+
+    if(isTX && isRX){
+        std::cerr << "Error: cannot set as both TX and RX" << std::endl;
+        return -1;
+    }
+
+    if(isTX){
+        CC::BufferHandler buf;
+        CC::UDP_TX udptx;
+
+        buf.Trigger(udptx);
+        udptx.Transmit();
+    }
+    else if(isRX){
+        std::cout << "RX not implemented yet" << std::endl;
+    }
 
     // Sleep for the specified interval
     //std::this_thread::sleep_for(std::chrono::seconds(intervalSeconds));
+    return 0;
 }
